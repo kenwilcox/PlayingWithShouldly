fdd8390690f5746f2516746afe1338bbb1da3c57
Ken Wilcox <wilcoxk@medamerica.com>
2015-07-02 16:34:20 -0600

    asserting exceptions

---
+++ PlayingWithShouldly.Tests/CalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+
+namespace PlayingWithShouldly.Tests
+{
+    public class CalculatorTests
+    {
+        public void ShouldThrowExceptionWhenDivideByZero()
+        {
+            var calculator = new Calculator();
+            Should.Throw<DivideByZeroException>(() => calculator.Divide(100, 0));
+        }
+
+        public void ShouldThrowExceptionWhenNumberTooBig()
+        {
+            var calculator = new Calculator();
+            var ex = Should.Throw<ArgumentOutOfRangeException>(() => calculator.Divide(101, 0));
+            ex.ParamName.ShouldBe("number");
+        }
+    }
+}
---
+++ PlayingWithShouldly/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayingWithShouldly
+{
+    public class Calculator
+    {
+        public int AddInts(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double AddDoubles(double a, double b)
+        {
+            return a + b;
+        }
+
+        public int Divide(int number, int by)
+        {
+            if (number > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number));
+            }
+
+            return number / by;
+        }
+    }
+}
