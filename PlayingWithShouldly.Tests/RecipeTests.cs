c17f62489d31ef55b094e266f33e10335b939d6d
Ken Wilcox <wilcoxk@medamerica.com>
2015-07-02 16:13:59 -0600

    Dictionary Should Contain Key and Value

---
+++ PlayingWithShouldly.Tests/RecipeTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+
+namespace PlayingWithShouldly.Tests
+{
+    public class RecipeTests
+    {
+        public void ShouldAddIngredients()
+        {
+            var applePie = new Recipe("Apple Pie");
+            applePie.AddIngredient("Apples", 200);
+            applePie.AddIngredient("Sugar", 50);
+            applePie.AddIngredient("Pastry", 300);
+
+            applePie.Ingredients.ShouldContainKey("Apples");
+            applePie.Ingredients.ShouldNotContainKey("Cherries");
+
+            applePie.Ingredients.ShouldContainKeyAndValue("Sugar", 50);
+            applePie.Ingredients.ShouldNotContainValueForKey("Sugar", 150);
+        }
+    }
+}
+
+
---
+++ PlayingWithShouldly/Recipe.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlayingWithShouldly
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+        public Dictionary<string, int> Ingredients { get; private set; } 
+
+        public Recipe(string name)
+        {
+            Name = name;
+            Ingredients = new Dictionary<string, int>();
+        }
+
+        public void AddIngredient(string ingredientName, int weight)
+        {
+            Ingredients.Add(ingredientName, weight);
+        }
+    }
+}
