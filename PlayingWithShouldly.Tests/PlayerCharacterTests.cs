8e37b1d96a126aff266084a71bd3533f247b7a48
Ken Wilcox <wilcoxk@medamerica.com>
2015-07-02 08:57:08 -0600

    Simple project

---
+++ PlayingWithShouldly.Tests/PlayerCharacterTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PlayingWithShouldly.Tests
+{
+    [TestFixture]
+    public class PlayerCharacterTests
+    {
+        [Test]
+        public void ShouldNotHaveAStaffOfWonder()
+        {
+            var playerCharacter = new PlayerCharacter();
+            Assert.That(playerCharacter.Weapons, Has.No.Member("Staff of Wonder"));
+        }
+    }
+}
---
+++ PlayingWithShouldly/PlayerCharacter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayingWithShouldly
+{
+    public class PlayerCharacter
+    {
+        public List<string> Weapons;
+         
+        public PlayerCharacter()
+        {
+            CreateStartingWeapons();
+        }
+
+        private void CreateStartingWeapons()
+        {
+            Weapons = new List<string>()
+            {
+                "Long Bow",
+                "Short Sword",
+                "Staff of Wonder"
+            };
+        }
+    }
+}
