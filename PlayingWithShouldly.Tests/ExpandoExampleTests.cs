78e5fcf61545e3714ba7deee8505d7916050a823
Ken Wilcox <wilcoxk@medamerica.com>
2015-07-02 16:50:36 -0600

    Asserting with dynamic objects

---
+++ PlayingWithShouldly.Tests/ExpandoExampleTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+
+namespace PlayingWithShouldly.Tests
+{
+    public class ExpandoExampleTests
+    {
+        public void ShouldHaveProperty()
+        {
+            dynamic person = new ExpandoObject();
+            person.Name = "Sarah";
+            person.Age = 27;
+
+            DynamicShould.HaveProperty(person, "Name");
+            DynamicShould.HaveProperty(person, "Age");
+        }
+    }
+}
