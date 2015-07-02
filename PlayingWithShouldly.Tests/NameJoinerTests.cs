e7d263d2d0ba56ff2aac2dff0fc20417e40ee27d
Ken Wilcox <wilcoxk@medamerica.com>
2015-07-02 10:53:53 -0600

    Added NameJoiner

---
+++ PlayingWithShouldly.Tests/NameJoinerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shouldly;
+
+namespace PlayingWithShouldly.Tests
+{
+    public class NameJoinerTests
+    {
+        public void ShouldJoinNamesCaseInsensitive()
+        {
+            var nameJoiner = new NameJoiner();
+            nameJoiner.JoinUpper("Sarah", "Connor").ShouldBe("Sarah Connor", Case.Insensitive);
+        }
+
+        public void ShouldJoinNamesCaseSensitive()
+        {
+            var nameJoiner = new NameJoiner();
+            nameJoiner.JoinUpper("Sarah", "Connor").ShouldBe("SARAH CONNOR", Case.Sensitive);
+        }
+
+        public void ShouldJoinNamesNotAMatch()
+        {
+            var nameJoiner = new NameJoiner();
+            nameJoiner.JoinUpper("Sarah", "Connor").ShouldNotBe("Sarah O'Connor");
+        }
+        public void ShouldJoinNamesNotAMatchCustomMessage()
+        {
+            var nameJoiner = new NameJoiner();
+            nameJoiner.JoinUpper("Sarah", "Connor").ShouldNotBe("SARAH O'CONNOR", "Just checking out custom messages");
+        }
+
+    }
+}
---
+++ PlayingWithShouldly/NameJoiner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayingWithShouldly
+{
+    public class NameJoiner
+    {
+        public string JoinUpper(string firstName, string lastName)
+        {
+            return firstName.ToUpperInvariant() + " " + lastName.ToUpperInvariant();
+        }
+
+    }
+}
