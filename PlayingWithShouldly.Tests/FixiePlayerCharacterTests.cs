ba8f058ed57efb1357c5da539c1f6f8b66a101f3
Ken Wilcox <wilcoxk@medamerica.com>
2015-07-02 09:24:38 -0600

    Fixie looks interesting

---
+++ PlayingWithShouldly.Tests/FixiePlayerCharacterTests.cs
@@ -0,0 +1,13 @@
+﻿using Shouldly;
+
+namespace PlayingWithShouldly.Tests
+{
+    class FixiePlayerCharacterTests
+    {
+        public void ShouldHaveASmallShield()
+        {
+            var playerCharacter = new PlayerCharacter();
+            playerCharacter.Weapons.ShouldContain("Small Shield");
+        }
+    }
+}
